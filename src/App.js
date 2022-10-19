e9dede80e4e5406f27b22e10fc6371f8efb8e60d
Mohamed Nasser <mmohammed@rasan.co>
2022-10-19 21:59:58 +0200

    layout_two_pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import { BookList } from './components/BookList';
 import { NotFound404 } from './components/common/NotFound404';
+import { BookSearch } from './components/BookSearch';
 export default  function App() {
   return (
     <div className="App">
-              <h1>eeeeee</h1>
              <Router>
             <Routes>
+             <Route exact path='/' element={< BookList />}></Route>
+             <Route exact path='/search' element={< BookSearch />}></Route>
              <Route exact path='/home' element={< Home />}></Route>
-             {/* <Route exact path='/search' element={< Search />}></Route> */}
               <Route path="/about" element={<About/>} />
                  {/* <Route path='*'element ={<NotFound404/>} ></Route>	   */}
             </Routes>
