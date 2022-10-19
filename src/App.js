80be33fd36d12651ddd6d862d329f7822f6041ae
Mohamed Nasser <mmohammed@rasan.co>
2022-10-19 02:10:02 +0200

    initi-layout-and_routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import { BookList } from './components/BookList';
+export default  function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <h1>eeeeee</h1>
+             <Router>
+            <Routes>
+             <Route exact path='/' element={< BookList />}></Route>
+             <Route exact path='/home' element={< Home />}></Route>
+             <Route exact path='/' element={< Home />}></Route>
+              <Route path="/about" element={<About/>} />
+                 {/* <Route path='*'element ={<NotFound404/>} ></Route>	    */}
+                 </Routes>
+        </Router>
+       </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
---
+++ src/components/BookList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export const BookList = () => { 
+  return (
+    <>
+      <h2>book list page</h2>
+    </>
+  );
+}
