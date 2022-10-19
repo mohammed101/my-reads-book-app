c973b0af6187d22acb13ee6c29851aa9e7fad912
Mohamed Nasser <mmohammed@rasan.co>
2022-10-19 23:18:25 +0200

    book shelf static

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BookShelf } from './BookShelf';
+
+const bookShelfsTypes = ['currentlyReading;','wantToRead','read'];
 export const BookList = () => {
   return (
     <div className='list-books'>
       <div className='list-books-title'>
       <h1>MyReads</h1>
     </div>
+    <BookShelf/>
+    <BookShelf/>
+    <BookShelf/>
     </div>
     
   );
---
+++ src/components/BookShelf.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export const BookShelf = () => {
+    return (
+        <div className='list-books-content'>
+      <h1>book shelf content</h1>
+    </div>
+    );
+}
